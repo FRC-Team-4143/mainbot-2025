12f0aa31980e6b5980e442b61c9fd141c25d639f
EthanTolan <e.j.tolan@students.mths.us>
2025-01-11 17:16:26 +0000

    Created feed/score commands + mapped to buttons

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -9,13 +9,15 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib.Util;
 import frc.robot.subsystems.*;
+import frc.robot.subsystems.CoralFunnel.FeedingMode;
 
 public abstract class OI {
 
   // Sets up both controllers
-  static CommandXboxController driver_joystick_ = new CommandXboxController(0);
+  static CommandXboxController driver_controller_ = new CommandXboxController(0);
 
   static SwerveDrivetrain swerve_drivetrain_ = SwerveDrivetrain.getInstance();
+  static CoralFunnel coral_funnel_ = CoralFunnel.getInstance();
 
   public static void configureBindings() {
 
@@ -29,14 +31,30 @@ public abstract class OI {
                     swerve_drivetrain_.seedFieldRelative(swerve_drivetrain_.getDriverPrespective()))
             .ignoringDisable(true));
 
-    driver_joystick_
+    driver_controller_
         .rightStick()
         .onTrue(
             Commands.runOnce(() -> swerve_drivetrain_.toggleFieldCentric(), swerve_drivetrain_));
+
+    driver_controller_
+        .leftTrigger()
+        .whileTrue(
+            Commands.startEnd(
+                () -> coral_funnel_.setFeedingMode(FeedingMode.FEEDING),
+                () -> coral_funnel_.setFeedingMode(FeedingMode.IDLE),
+                coral_funnel_));
+
+    driver_controller_
+        .rightTrigger()
+        .whileTrue(
+            Commands.startEnd(
+                () -> coral_funnel_.setFeedingMode(FeedingMode.SCORING),
+                () -> coral_funnel_.setFeedingMode(FeedingMode.IDLE),
+                coral_funnel_));
   }
 
   public static double getDriverJoystickLeftX() {
-    double val = driver_joystick_.getLeftX();
+    double val = driver_controller_.getLeftX();
     double output = val * val;
     output = Math.copySign(output, val);
     // return output;
@@ -44,7 +62,7 @@ public abstract class OI {
   }
 
   public static double getDriverJoystickLeftY() {
-    double val = driver_joystick_.getLeftY();
+    double val = driver_controller_.getLeftY();
     double output = val * val;
     output = Math.copySign(output, val);
     // return output;
@@ -52,7 +70,7 @@ public abstract class OI {
   }
 
   public static double getDriverJoystickRightX() {
-    double val = driver_joystick_.getRightX();
+    double val = driver_controller_.getRightX();
     double output = val * val;
     output = Math.copySign(output, val);
     // return output;
@@ -60,11 +78,11 @@ public abstract class OI {
   }
 
   public static boolean getDriverJoystickRightY() {
-    double val = driver_joystick_.getRightY();
+    double val = driver_controller_.getRightY();
     return Util.epislonEquals(val, 0, 0.1);
   }
 
   public static double getDriverJoystickPOVangle() {
-    return driver_joystick_.getHID().getPOV();
+    return driver_controller_.getHID().getPOV();
   }
 }
--- src/main/java/frc/robot/subsystems/CoralFunnel.java
+++ src/main/java/frc/robot/subsystems/CoralFunnel.java
@@ -4,150 +4,142 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkBase;
-import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.config.SparkFlexConfig;
 import frc.lib.subsystem.Subsystem;
 import frc.robot.Constants.FeederConstants;
-import monologue.Logged;
 import monologue.Annotations.Log;
+import monologue.Logged;
 
 public class CoralFunnel extends Subsystem {
 
-    private SparkFlex left_feeder_motor_;
-    private SparkFlex right_feeder_motor_;
+  private SparkFlex left_feeder_motor_;
+  private SparkFlex right_feeder_motor_;
 
-    public enum FeedingMode {
-        IDLE,
-        FEEDING
+  public enum FeedingMode {
+    IDLE,
+    FEEDING,
+    SCORING
+  }
+
+  // Singleton pattern
+  private static CoralFunnel example_instance = null;
+
+  // C
+  public static CoralFunnel getInstance() {
+    if (example_instance == null) {
+      example_instance = new CoralFunnel();
+    }
+    return example_instance;
+  }
+
+  /** Class Members */
+  private CoralFunnelPeriodicIo io_;
+
+  private CoralFunnel() {
+    // Create io object first in subsystem configuration
+    io_ = new CoralFunnelPeriodicIo();
+    left_feeder_motor_ = new SparkFlex(FeederConstants.LEFT_FEEDER_MOTOR, MotorType.kBrushless);
+    right_feeder_motor_ = new SparkFlex(FeederConstants.RIGHT_FEEDER_MOTOR, MotorType.kBrushless);
+
+    SparkFlexConfig config_ = new SparkFlexConfig();
+    config_.inverted(FeederConstants.LEFT_FEEDER_INVERTED);
+    left_feeder_motor_.configure(
+        config_,
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters);
+    config_.inverted(FeederConstants.RIGHT_FEEDER_INVERTED);
+    right_feeder_motor_.configure(
+        config_,
+        SparkBase.ResetMode.kResetSafeParameters,
+        SparkBase.PersistMode.kPersistParameters);
+
+    // Call reset last in subsystem configuration
+    reset();
+  }
+
+  /**
+   * This function should be logic and code to fully reset your subsystem. This is called during
+   * initialization, and should handle I/O configuration and initializing data members.
+   */
+  @Override
+  public void reset() {}
+
+  /**
+   * Inside this function, all of the SENSORS should be read into variables stored in the PeriodicIO
+   * class defined below. There should be no calls to output to actuators, or any logic within this
+   * function.
+   */
+  @Override
+  public void readPeriodicInputs(double timestamp) {
+    io_.left_current_sensor_ = left_feeder_motor_.getOutputCurrent();
+    io_.right_current_sensor_ = right_feeder_motor_.getOutputCurrent();
+  }
+
+  /**
+   * Inside this function, all of the LOGIC should compute updates to output variables in the
+   * PeriodicIO class defined below. There should be no calls to read from sensors or write to
+   * actuators in this function.
+   */
+  @Override
+  public void updateLogic(double timestamp) {
+    switch (io_.feeding_mode_) {
+      case FEEDING:
+        io_.feeder_output_ = FeederConstants.FEEDER_SPEED;
+        break;
+
+      case SCORING:
+        io_.feeder_output_ = FeederConstants.SCORE_SPEED;
+
+      case IDLE:
+      default:
+        io_.feeder_output_ = FeederConstants.IDLE_SPEED;
+        break;
     }
 
-    // Singleton pattern
-    private static CoralFunnel example_instance = null;
+    io_.average_motor_current_ = (io_.left_current_sensor_ + io_.right_current_sensor_) / 2;
+  }
 
-    // C
-    public static CoralFunnel getInstance() {
-        if (example_instance == null) {
-            example_instance = new CoralFunnel();
-        }
-        return example_instance;
-    }
+  /**
+   * Inside this function actuator OUTPUTS should be updated from data contained in the PeriodicIO
+   * class defined below. There should be little to no logic contained within this function, and no
+   * sensors should be read.
+   */
+  @Override
+  public void writePeriodicOutputs(double timestamp) {
+    left_feeder_motor_.set(io_.feeder_output_);
+    right_feeder_motor_.set(io_.feeder_output_);
+  }
 
-    /**
-     * Class Members
-     */
-    private CoralFunnelPeriodicIo io_;
+  /**
+   * Inside this function telemetry should be output to smartdashboard. The data should be collected
+   * out of the PeriodicIO class instance defined below. There should be no sensor information read
+   * in this function nor any outputs made to actuators within this function. Only publish to
+   * smartdashboard here.
+   */
+  @Override
+  public void outputTelemetry(double timestamp) {}
 
-    private CoralFunnel() {
-        // Create io object first in subsystem configuration
-        io_ = new CoralFunnelPeriodicIo();
-        left_feeder_motor_ = new SparkFlex(FeederConstants.LEFT_FEEDER_MOTOR, MotorType.kBrushless);
-        right_feeder_motor_ = new SparkFlex(FeederConstants.RIGHT_FEEDER_MOTOR, MotorType.kBrushless);
+  public class CoralFunnelPeriodicIo implements Logged {
+    @Log.File public double feeder_output_ = 0;
+    @Log.File public double left_current_sensor_ = 0;
+    @Log.File public double right_current_sensor_ = 0;
+    @Log.File public FeedingMode feeding_mode_ = FeedingMode.IDLE;
+    @Log.File public double average_motor_current_ = 0;
+  }
 
-        SparkFlexConfig config_ = new SparkFlexConfig();
-        config_.inverted(FeederConstants.LEFT_FEEDER_INVERTED);
-        left_feeder_motor_.configure(config_, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-        config_.inverted(FeederConstants.RIGHT_FEEDER_INVERTED);
-        right_feeder_motor_.configure(config_, SparkBase.ResetMode.kResetSafeParameters, SparkBase.PersistMode.kPersistParameters);
-       
+  public void setFeedingMode(FeedingMode mode) {
+    io_.feeding_mode_ = mode;
+  }
 
-        // Call reset last in subsystem configuration
-        reset();
-    }
+  public boolean hasCoral() {
+    return io_.average_motor_current_ > 50;
+  }
 
-    /**
-     * This function should be logic and code to fully reset your subsystem.
-     * This is called during initialization, and should handle I/O configuration and
-     * initializing data members.
-     */
-    @Override
-    public void reset() {
-
-    }
-
-    /**
-     * Inside this function, all of the SENSORS should be read into variables
-     * stored in the PeriodicIO class defined below. There should be no calls
-     * to output to actuators, or any logic within this function.
-     */
-    @Override
-    public void readPeriodicInputs(double timestamp) {
-        io_.left_current_sensor_ = left_feeder_motor_.getOutputCurrent();
-        io_.right_current_sensor_ = right_feeder_motor_.getOutputCurrent();
-    }
-
-    /**
-     * Inside this function, all of the LOGIC should compute updates to output
-     * variables in the PeriodicIO class defined below. There should be no
-     * calls to read from sensors or write to actuators in this function.
-     */
-    @Override
-    public void updateLogic(double timestamp) {
-        switch (io_.feeding_mode) {
-
-            case FEEDING:
-
-                io_.feeder_output_ = 1;
-                break;
-            case IDLE:
-            default:
-                io_.feeder_output_ = 0;
-                break;
-        }
-
-        io_.average_motor_current_ = (io_.left_current_sensor_ + io_.right_current_sensor_)/2;
-
-    }
-
-    /**
-     * Inside this function actuator OUTPUTS should be updated from data contained
-     * in the PeriodicIO class
-     * defined below. There should be little to no logic contained within this
-     * function, and no sensors
-     * should be read.
-     */
-    @Override
-    public void writePeriodicOutputs(double timestamp) {
-        left_feeder_motor_.set(io_.feeder_output_);
-        right_feeder_motor_.set(io_.feeder_output_);
-
-    }
-
-    /**
-     * Inside this function telemetry should be output to smartdashboard. The data
-     * should be collected out
-     * of the PeriodicIO class instance defined below. There should be no sensor
-     * information read in this function nor
-     * any outputs made to actuators within this function. Only publish to
-     * smartdashboard here.
-     */
-    @Override
-    public void outputTelemetry(double timestamp) {
-    }
-
-    public class CoralFunnelPeriodicIo implements Logged {
-        @Log.File
-        public double feeder_output_ = 0;
-        @Log.File
-        public double left_current_sensor_ = 0;
-        @Log.File
-        public double right_current_sensor_ = 0;
-        @Log.File
-        public FeedingMode feeding_mode = FeedingMode.IDLE;
-        @Log.File
-        public double average_motor_current_ = 0;
-
-    }
-    public void setFeedingMode(FeedingMode mode) {
-        io_.feeding_mode = mode;
-    }
-
-    @Override
-    public Logged getLoggingObject() {
-        return io_;
-    }
+  @Override
+  public Logged getLoggingObject() {
+    return io_;
+  }
 }
