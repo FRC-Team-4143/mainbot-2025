679256e996cb1a9c7b149e40eea222e7d1a18922
Ben-Shoff <160341722+Ben-Shoff@users.noreply.github.com>
2025-01-15 02:05:31 +0000

    Worked on Climber Subsystem Structure (not finished)

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.AnalogInput;
 import frc.lib.subsystem.Subsystem;
@@ -15,13 +17,12 @@ public class Climber extends Subsystem {
   enum climberStage {
     RETRACTED,
     CLAMPED,
-    EXTENDED,
     AIRBORNE
   }
 
-  TalonFX climber_motor_ = new TalonFX(0);
-  TalonFX latch = new TalonFX(1);
-  AnalogInput climber_encoder_ = new AnalogInput(0);
+  TalonFX climber_motor_;
+  TalonFX clamper;
+  AnalogInput climber_encoder_;
   // Singleton pattern
   private static Climber example_instance = null;
 
@@ -39,9 +40,17 @@ public class Climber extends Subsystem {
   private Climber() {
     // Create io object first in subsystem configuration
     io_ = new ClimberPeriodicIo();
-
+    climber_motor_ = new TalonFX(0);
+    clamper = new TalonFX(1);
+    climber_encoder_ = new AnalogInput(0);
     // Call reset last in subsystem configuration
     reset();
+
+    var slot0configs = new Slot0Configs();
+
+    slot0configs.kP = 0; 
+    slot0configs.kI = 0; 
+    slot0configs.kD = 0; 
   }
 
   /**
@@ -60,6 +69,9 @@ public class Climber extends Subsystem {
   public void readPeriodicInputs(double timestamp) {
     // read climber motor
     // ./climber_encoder_.getValue();
+    io_.climber_rotational_pose_ += climber_encoder_.getValue();
+    io_.clamper_current_ = clamper.getSupplyCurrent().getValueAsDouble();
+    
   }
 
   /**
@@ -68,15 +80,35 @@ public class Climber extends Subsystem {
    * actuators in this function.
    */
   @Override
-  public void updateLogic(double timestamp) {}
-
+  public void updateLogic(double timestamp) {
+    switch(io_.climber_stage_){
+      case RETRACTED:
+        io_.climber_vertical_target_ = 0;   //Retracted position
+        io_.climber_rotational_target_ = 0;   //Retracted position
+        io_.clamper_target_ = 0;  //Retracted position
+      break;
+      case CLAMPED:
+        io_.climber_vertical_target_ = 0;   //Extended to grab deep cage
+        io_.climber_rotational_target_ = 0;   //Rotated to grab deep cacge
+        io_.clamper_target_ = 0;  //Clamped
+      break;
+      case AIRBORNE:
+        io_.climber_vertical_target_ = 0;   //Likely identical to clamped
+        io_.climber_rotational_target_ = 0;   //Rotated off the ground
+        io_.clamper_target_ = 0;  //Clamped
+      break;
+    }
+  }
   /**
    * Inside this function actuator OUTPUTS should be updated from data contained in the PeriodicIO
    * class defined below. There should be little to no logic contained within this function, and no
    * sensors should be read.
    */
   @Override
-  public void writePeriodicOutputs(double timestamp) {}
+  public void writePeriodicOutputs(double timestamp) {
+    climber_motor_.set(io_.climber_rotational_target_);
+    clamper.set(io_.clamper_target_);
+  }
 
   /**
    * Inside this function telemetry should be output to smartdashboard. The data should be collected
@@ -88,13 +120,12 @@ public class Climber extends Subsystem {
   public void outputTelemetry(double timestamp) {}
 
   public class ClimberPeriodicIo implements Logged {
-    @Log.File private double climber_y_pose_;
     @Log.File private double climber_rotational_pose_;
-    @Log.File private double clamper_pose_;
     @Log.File private double clamper_current_;
-    @Log.File private double climber_vertical_target;
-    @Log.File private double climber_rotational_target;
-    @Log.File private double clamper_target;
+    @Log.File private double climber_vertical_target_;
+    @Log.File private double climber_rotational_target_;
+    @Log.File private double clamper_target_;
+    @Log.File private climberStage climber_stage_;
   }
 
   @Override
