5d36df9e38b24f8ac04afff9dab9b8b3bdbd0a36
Armolad <n.p.evans@students.mths.us>
2025-01-28 19:45:30 -0600

    Added Region Interface

--- src/main/java/frc/mw_lib/geometry/CircularRegion.java
+++ src/main/java/frc/mw_lib/geometry/CircularRegion.java
@@ -5,49 +5,45 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
-import java.awt.geom.*;
 
 /**
- * This class models a region of the field. It is defined by its vertices and the transition points
- * to neighboring regions.
+ * This class models a region of the field. It is defined by its center and radius
  */
-public class CircularRegion {
-  private Path2D shape_;
+public class CircularRegion implements Region {
   private Translation2d center_;
   private double radius_;
-  private String region_name_;
 
   /**
-   * Create a Region2d, a polygon, from an array of Translation2d specifying vertices of a polygon.
+   * Create a Region2d, a polygon, from an array of Translation2d specifying
+   * vertices of a polygon.
    * The polygon is created using the even-odd winding rule.
    *
-   * @param points the array of Translation2d that define the vertices of the region.
+   * @param points     the array of Translation2d that define the vertices of the
+   *                   region.
    * @param regionName the name of the region that is used for logging
    */
   public CircularRegion(Translation2d center_, double radius_, String region_name_) {
     this.center_ = center_;
     this.radius_ = radius_;
-    this.region_name_ = region_name_;
 
-    logPoints();
+    logPoints(new Translation2d[] {}, region_name_);
   }
 
   /**
-   * Log the bounding rectangle of the region and the transition points to neighboring regions.
-   * These can be visualized using AdvantageScope to confirm that the regions are properly defined.
+   * Log the circumference of the circle in 360 points.
+   * These can be visualized using AdvantageScope to confirm that the regions are
+   * properly defined.
    */
-  public void logPoints() {
+  public void logPoints(Translation2d[] point, String regionName) {
     Translation2d[] points = new Translation2d[360];
     for (int i = 0; i < 360; i++) {
-      points[i] =
-          center_
-              .plus(new Translation2d(radius_, 0))
-              .rotateAround(center_, Rotation2d.fromDegrees(i));
+      points[i] = center_
+          .plus(new Translation2d(radius_, 0))
+          .rotateAround(center_, Rotation2d.fromDegrees(i));
     }
-    StructArrayPublisher<Translation2d> arrayPublisher =
-        NetworkTableInstance.getDefault()
-            .getStructArrayTopic(region_name_, Translation2d.struct)
-            .publish();
+    StructArrayPublisher<Translation2d> arrayPublisher = NetworkTableInstance.getDefault()
+        .getStructArrayTopic(regionName, Translation2d.struct)
+        .publish();
     arrayPublisher.set(points);
   }
 
--- src/main/java/frc/mw_lib/geometry/PolygonRegion.java
+++ src/main/java/frc/mw_lib/geometry/PolygonRegion.java
@@ -7,17 +7,18 @@ import edu.wpi.first.networktables.StructArrayPublisher;
 import java.awt.geom.*;
 
 /**
- * This class models a region of the field. It is defined by its vertices and the transition points
- * to neighboring regions.
+ * This class models a region of the field. It is defined by its vertices
  */
-public class PolygonRegion {
+public class PolygonRegion implements Region {
   private Path2D shape;
 
   /**
-   * Create a Region2d, a polygon, from an array of Translation2d specifying vertices of a polygon.
+   * Create a Region2d, a polygon, from an array of Translation2d specifying
+   * vertices of a polygon.
    * The polygon is created using the even-odd winding rule.
    *
-   * @param points the array of Translation2d that define the vertices of the region.
+   * @param points     the array of Translation2d that define the vertices of the
+   *                   region.
    * @param regionName the name of the region that is used for logging
    */
   public PolygonRegion(Translation2d[] points, String regionName) {
@@ -33,15 +34,15 @@ public class PolygonRegion {
   }
 
   /**
-   * Log the bounding rectangle of the region and the transition points to neighboring regions.
-   * These can be visualized using AdvantageScope to confirm that the regions are properly defined.
+   * Log the bounding points of the region.
+   * These can be visualized using AdvantageScope to confirm that the regions are
+   * properly defined.
    */
   public void logPoints(Translation2d[] points, String regionName) {
 
-    StructArrayPublisher<Translation2d> arrayPublisher =
-        NetworkTableInstance.getDefault()
-            .getStructArrayTopic(regionName, Translation2d.struct)
-            .publish();
+    StructArrayPublisher<Translation2d> arrayPublisher = NetworkTableInstance.getDefault()
+        .getStructArrayTopic(regionName, Translation2d.struct)
+        .publish();
     arrayPublisher.set(points);
   }
 
