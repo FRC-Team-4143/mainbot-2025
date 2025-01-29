c3bce6400d618f9d8c4dddfd55693ac7cb73d9cd
Cole Hunt <huntcr2001@gmail.com>
2025-01-29 11:13:40 -0600

    adds bindSetpoint and updates smartdash formatting

--- src/main/java/frc/mw_lib/controls/TalonFXTuner.java
+++ src/main/java/frc/mw_lib/controls/TalonFXTuner.java
@@ -8,12 +8,13 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger
 
 public class TalonFXTuner {
 
   TalonFX motor_;
   TalonFX[] follower_motors_;
-  String system_name_;
+  String system_name_ = "TalonFXTuner - ";
   Slot0Configs gains_;
 
   /**
@@ -27,7 +28,7 @@ public class TalonFXTuner {
   public TalonFXTuner(TalonFX motor, TalonFX[] followers, String system_name) {
     motor_ = motor;
     follower_motors_ = followers;
-    system_name_ = system_name;
+    system_name_ += (system_name + "/");
     gains_ = new Slot0Configs();
     // Load current gains from motor controller to class object
     motor_.getConfigurator().refresh(gains_);
@@ -44,13 +45,13 @@ public class TalonFXTuner {
 
   /** Reads all gains from SmartDashboard tuning group and updates motor config */
   private void updateGains() {
-    gains_.kP = SmartDashboard.getNumber(system_name_ + " - P", gains_.kP);
-    gains_.kI = SmartDashboard.getNumber(system_name_ + " - I", gains_.kI);
-    gains_.kD = SmartDashboard.getNumber(system_name_ + " - D", gains_.kD);
-    gains_.kS = SmartDashboard.getNumber(system_name_ + " - S", gains_.kS);
-    gains_.kV = SmartDashboard.getNumber(system_name_ + " - V", gains_.kV);
-    gains_.kA = SmartDashboard.getNumber(system_name_ + " - A", gains_.kA);
-    gains_.kG = SmartDashboard.getNumber(system_name_ + " - G", gains_.kG);
+    gains_.kP = SmartDashboard.getNumber(system_name_ + "P", gains_.kP);
+    gains_.kI = SmartDashboard.getNumber(system_name_ + "I", gains_.kI);
+    gains_.kD = SmartDashboard.getNumber(system_name_ + "D", gains_.kD);
+    gains_.kS = SmartDashboard.getNumber(system_name_ + "S", gains_.kS);
+    gains_.kV = SmartDashboard.getNumber(system_name_ + "V", gains_.kV);
+    gains_.kA = SmartDashboard.getNumber(system_name_ + "A", gains_.kA);
+    gains_.kG = SmartDashboard.getNumber(system_name_ + "G", gains_.kG);
     motor_.getConfigurator().apply(gains_);
     // Clears control request after gains are updated for safety
     clearSetpoint();
@@ -60,9 +61,9 @@ public class TalonFXTuner {
    * Updates the control request being applied to the motor
    *
    * @param request supports any ctre control request type, but is intened for ClosedLoop types
-   * @return
+   * @return command that set the setpoint when onTrue and clears the setpoint when interrupted
    */
-  public Command updateSetpoint(ControlRequest request) {
+  private Command updateSetpoint(ControlRequest request) {
     return Commands.startEnd(
         () -> {
           motor_.setControl(request);
@@ -75,6 +76,16 @@ public class TalonFXTuner {
         () -> clearSetpoint());
   }
 
+  /**
+   * Binds the given trigger to execute a setpoint update of type request
+   *
+   * @param request supports any ctre control request type, but is intened for ClosedLoop types
+   * @param trigger wpilib trigger to start/stop the execution of the setpoint command
+  */
+  public void bindSetpoint(ControlRequest request, Trigger trigger) {
+    trigger.whileTrue(updateSetpoint(request));
+  }
+
   /** Set the active motor request to 0 voltage to act as quick disable */
   private void clearSetpoint() {
     motor_.setControl(new VoltageOut(0));
@@ -82,13 +93,13 @@ public class TalonFXTuner {
 
   /** Adds all tunable values to SmartDashboard under the system_name */
   private void setupDashboard() {
-    SmartDashboard.putNumber(system_name_ + " - P", gains_.kP);
-    SmartDashboard.putNumber(system_name_ + " - I", gains_.kI);
-    SmartDashboard.putNumber(system_name_ + " - D", gains_.kD);
-    SmartDashboard.putNumber(system_name_ + " - S", gains_.kS);
-    SmartDashboard.putNumber(system_name_ + " - V", gains_.kV);
-    SmartDashboard.putNumber(system_name_ + " - A", gains_.kA);
-    SmartDashboard.putNumber(system_name_ + " - G", gains_.kG);
-    SmartDashboard.putData(system_name_ + " - Update", Commands.runOnce(() -> updateGains()));
+    SmartDashboard.putNumber(system_name_ + "P", gains_.kP);
+    SmartDashboard.putNumber(system_name_ + "I", gains_.kI);
+    SmartDashboard.putNumber(system_name_ + "D", gains_.kD);
+    SmartDashboard.putNumber(system_name_ + "S", gains_.kS);
+    SmartDashboard.putNumber(system_name_ + "V", gains_.kV);
+    SmartDashboard.putNumber(system_name_ + "A", gains_.kA);
+    SmartDashboard.putNumber(system_name_ + "G", gains_.kG);
+    SmartDashboard.putData(system_name_ + "Update", Commands.runOnce(() -> updateGains()));
   }
 }
