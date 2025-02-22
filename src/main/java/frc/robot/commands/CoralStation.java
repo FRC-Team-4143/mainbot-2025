4d35ef82e8fd0f3bf0962a7d5297d25139a80284
Armolad <n.p.evans@students.mths.us>
2025-02-22 12:58:03 -0600

    set up rotation to coral station

--- src/main/java/frc/robot/commands/CoralStation.java
+++ src/main/java/frc/robot/commands/CoralStation.java
@@ -4,10 +4,14 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.lib.FieldRegions;
 import frc.robot.Constants;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Elevator.SpeedLimit;
+import frc.robot.subsystems.PoseEstimator;
+import frc.robot.subsystems.SwerveDrivetrain;
 
 public class CoralStation extends Command {
 
@@ -28,7 +32,24 @@ public class CoralStation extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    //if in zone
+    if(FieldRegions.STATION_REGIONS[1].contains(PoseEstimator.getInstance().getRobotPose())){
+      Rotation2d rightTarget = FieldRegions.REGION_POSE_TABLE.get(FieldRegions.STATION_REGIONS[1].getName()).getRotation(); 
+      SwerveDrivetrain.getInstance().setTargetRotation(rightTarget);
+    }
+    else if(FieldRegions.STATION_REGIONS[0].contains(PoseEstimator.getInstance().getRobotPose())){
+      Rotation2d leftTarget = FieldRegions.REGION_POSE_TABLE.get(FieldRegions.STATION_REGIONS[0].getName()).getRotation();
+      SwerveDrivetrain.getInstance().setTargetRotation(leftTarget);
+    }
+    else{
+      SwerveDrivetrain.getInstance().restoreDefaultDriveMode(); 
+    }
+    //then pull tag rotation
+
+    //then set target angle based on rotation
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
