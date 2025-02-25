c3f871c0206c028316644b7d8ddf5a3cf484ac70
ColeHunt <huntcr2001@gmail.com>
2025-02-25 12:30:13 -0600

    adds MK4I+ types

--- src/main/java/frc/mw_lib/swerve/utility/ModuleType.java
+++ src/main/java/frc/mw_lib/swerve/utility/ModuleType.java
@@ -2,8 +2,6 @@ package frc.mw_lib.swerve.utility;
 
 import java.util.Hashtable;
 
-import frc.mw_lib.geometry.Region;
-
 public class ModuleType {
     public final String name;
     public final double steerRatio;
@@ -16,10 +14,13 @@ public class ModuleType {
     }
 
     // MK4I
-    public static final ModuleType MK4I_L1 = new ModuleType("MK4I-L1", 150 / 7.0, 8.14);
-    public static final ModuleType MK4I_L2 = new ModuleType("MK4I-L2", 150 / 7.0, 6.75);
-    public static final ModuleType MK4I_L3 = new ModuleType("MK4I-L3", 150 / 7.0, 6.12);
-    public static final ModuleType MK4I_L4 = new ModuleType("MK4I-L4", 150 / 7.0, 5.14);
+    public static final ModuleType MK4I_L1 = new ModuleType("MK4I-L1", 150.0 / 7.0, 8.14);
+    public static final ModuleType MK4I_L2 = new ModuleType("MK4I-L2", 150.0 / 7.0, 6.75);
+    public static final ModuleType MK4I_L3 = new ModuleType("MK4I-L3", 150.0 / 7.0, 6.12);
+    public static final ModuleType MK4I_L4 = new ModuleType("MK4I-L4", 150.0 / 7.0, 5.14);
+    public static final ModuleType MK4I_L1_PLUS = new ModuleType("MK4I-L1", 150 / 7.0, 7.13);
+    public static final ModuleType MK4I_L2_PLUS = new ModuleType("MK4I-L2", 150 / 7.0, 5.9);
+    public static final ModuleType MK4I_L3_PLUS = new ModuleType("MK4I-L3", 150 / 7.0, 5.36);
 
     // MK4N
     public static final ModuleType MK4N_L1_PLUS = new ModuleType("MK4N-L1+", 18.75, 7.13);
