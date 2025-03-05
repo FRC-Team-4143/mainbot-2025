a4ed2d2abf673460f13d9c5f7dcb024a68def012
ColeHunt <huntcr2001@gmail.com>
2025-03-04 18:26:29 -0600

    updates variable names and removes unused

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -198,7 +198,7 @@ public class Elevator extends Subsystem {
   public void readPeriodicInputs(double timestamp) {
     io_.elevator_follower_rotations_ = elevator_follower_.getPosition().getValue().in(Rotations);
     io_.elevator_master_rotations_ = elevator_master_.getPosition().getValue().in(Rotations);
-    io_.current_elevator_height =
+    io_.current_elevator_height_ =
         (((io_.elevator_master_rotations_ + io_.elevator_follower_rotations_) / 2)
                 * ElevatorConstants.ELEVATOR_ROTATIONS_TO_METERS)
             + ElevatorConstants.ELEVATOR_MIN_HEIGHT;
@@ -209,31 +209,31 @@ public class Elevator extends Subsystem {
 
   /** Computes updated outputs for the actuators */
   public void updateLogic(double timestamp) {
-    switch (io_.current_control_mode) {
+    switch (io_.current_control_mode_) {
       case END_EFFECTOR:
-        io_.target_elevator_height =
-            kinematics_.desiredElevatorZ(io_.target_arm_height, io_.target_arm_angle);
+        io_.target_elevator_height_ =
+            kinematics_.desiredElevatorZ(io_.target_arm_height_, io_.target_arm_angle_);
         break;
       case PIVOT:
         // target_elevator_height is set by external commands
         break;
     }
 
-    if (io_.target_elevator_height < ElevatorConstants.ELEVATOR_MIN_HEIGHT) {
+    if (io_.target_elevator_height_ < ElevatorConstants.ELEVATOR_MIN_HEIGHT) {
       DataLogManager.log(
           "ERROR: Target Elevator Height: "
-              + io_.target_elevator_height
+              + io_.target_elevator_height_
               + " Min Elevator Height: "
               + ElevatorConstants.ELEVATOR_MIN_HEIGHT);
-      io_.target_elevator_height = ElevatorConstants.ELEVATOR_MIN_HEIGHT;
+      io_.target_elevator_height_ = ElevatorConstants.ELEVATOR_MIN_HEIGHT;
     }
-    if (io_.target_elevator_height > ElevatorConstants.ELEVATOR_MAX_HEIGHT) {
+    if (io_.target_elevator_height_ > ElevatorConstants.ELEVATOR_MAX_HEIGHT) {
       DataLogManager.log(
           "ERROR: Target Elevator Height: "
-              + io_.target_elevator_height
+              + io_.target_elevator_height_
               + " Min Elevator Height: "
               + ElevatorConstants.ELEVATOR_MAX_HEIGHT);
-      io_.target_elevator_height = ElevatorConstants.ELEVATOR_MAX_HEIGHT;
+      io_.target_elevator_height_ = ElevatorConstants.ELEVATOR_MAX_HEIGHT;
     }
   }
 
@@ -242,34 +242,34 @@ public class Elevator extends Subsystem {
     elevator_master_.setControl(
         elevator_request_
             .withPosition(
-                ((io_.target_elevator_height + io_.elevator_offset_)
+                ((io_.target_elevator_height_ + io_.elevator_offset_)
                         - ElevatorConstants.ELEVATOR_MIN_HEIGHT)
                     / ElevatorConstants.ELEVATOR_ROTATIONS_TO_METERS)
             .withLimitReverseMotion(isElevatorAtMinimum()));
     elevator_follower_.setControl(new StrictFollower(elevator_master_.getDeviceID()));
     arm_motor_.setControl(
-        arm_request_.withPosition(io_.target_arm_angle.rotateBy(io_.arm_offset_).getRotations()));
+        arm_request_.withPosition(io_.target_arm_angle_.rotateBy(io_.arm_offset_).getRotations()));
   }
 
   /** Outputs all logging information to the SmartDashboard */
   public void outputTelemetry(double timestamp) {
     SmartDashboard.putString(
-        "Subsystems/Elevator/Control Mode", io_.current_control_mode.toString());
-    SmartDashboard.putNumber("Subsystems/Elevator/Target Height", io_.target_elevator_height);
-    SmartDashboard.putNumber("Subsystems/Elevator/Current Height", io_.current_elevator_height);
+        "Subsystems/Elevator/Control Mode", io_.current_control_mode_.toString());
+    SmartDashboard.putNumber("Subsystems/Elevator/Target Height", io_.target_elevator_height_);
+    SmartDashboard.putNumber("Subsystems/Elevator/Current Height", io_.current_elevator_height_);
     SmartDashboard.putNumber("Subsystems/Elevator/Manual Offset", io_.elevator_offset_);
     SmartDashboard.putString(
         "Subsystems/Elevator/Motor Temp Master", elevator_master_.getDeviceTemp().toString());
     SmartDashboard.putString(
         "Subsystems/Elevator/Motor Temp Follower", elevator_follower_.getDeviceTemp().toString());
 
-    SmartDashboard.putString("Subsystems/Arm/Control Mode", io_.current_control_mode.toString());
+    SmartDashboard.putString("Subsystems/Arm/Control Mode", io_.current_control_mode_.toString());
     SmartDashboard.putNumber("Subsystems/Arm/Current Angle", io_.current_arm_angle_);
-    SmartDashboard.putNumber("Subsystems/Arm/Target Angle", io_.target_arm_angle.getRadians());
+    SmartDashboard.putNumber("Subsystems/Arm/Target Angle", io_.target_arm_angle_.getRadians());
     SmartDashboard.putNumber(
         "Subsystems/Arm/Current Height",
-        kinematics_.effectorZ(io_.current_elevator_height, io_.current_arm_angle_));
-    SmartDashboard.putNumber("Subsystems/Arm/Target Height", io_.target_arm_height);
+        kinematics_.effectorZ(io_.current_elevator_height_, io_.current_arm_angle_));
+    SmartDashboard.putNumber("Subsystems/Arm/Target Height", io_.target_arm_height_);
     SmartDashboard.putNumber(
         "Subsystems/Arm/Height Offset", kinematics_.calZOffset(io_.current_arm_angle_));
     SmartDashboard.putNumber(
@@ -286,16 +286,16 @@ public class Elevator extends Subsystem {
           new Pose3d(
               0,
               0,
-              (io_.current_elevator_height - ElevatorConstants.ELEVATOR_MIN_HEIGHT) / 2
+              (io_.current_elevator_height_ - ElevatorConstants.ELEVATOR_MIN_HEIGHT) / 2
                   + ElevatorConstants.ELEVATOR_MIN_HEIGHT,
               new Rotation3d()),
-          new Pose3d(0, 0, io_.current_elevator_height, new Rotation3d())
+          new Pose3d(0, 0, io_.current_elevator_height_, new Rotation3d())
         });
     arm_pub_.set(
         new Pose3d(
             0,
             0,
-            io_.current_elevator_height + 0.012,
+            io_.current_elevator_height_ + 0.012,
             new Rotation3d(0, io_.current_arm_angle_, 0)));
   }
 
@@ -305,7 +305,7 @@ public class Elevator extends Subsystem {
   public boolean isArmAtTarget() {
     return Util.epislonEquals(
         io_.current_arm_angle_,
-        io_.target_arm_angle.getRadians(),
+        io_.target_arm_angle_.getRadians(),
         ElevatorConstants.ARM_TARGET_THRESHOLD);
   }
 
@@ -314,8 +314,8 @@ public class Elevator extends Subsystem {
    */
   public boolean isElevatorAtTarget() {
     return Util.epislonEquals(
-        io_.current_elevator_height,
-        io_.target_elevator_height,
+        io_.current_elevator_height_,
+        io_.target_elevator_height_,
         ElevatorConstants.ELEVATOR_TARGET_THRESHOLD);
   }
 
@@ -337,7 +337,7 @@ public class Elevator extends Subsystem {
    * @return If the elevator is within threshold of the zero pose
    */
   public boolean isElevatorNearLimitSwitch() {
-    return io_.current_elevator_height <= ElevatorConstants.ELEVATOR_ZERO_THRESHOLD;
+    return io_.current_elevator_height_ <= ElevatorConstants.ELEVATOR_ZERO_THRESHOLD;
   }
 
   /** Reset the elevator pos to zero */
@@ -363,16 +363,16 @@ public class Elevator extends Subsystem {
 
   /** */
   private void setPivotHeight(double height, Rotation2d angle) {
-    io_.current_control_mode = ControlMode.PIVOT;
-    io_.target_arm_angle = angle;
-    io_.target_elevator_height = height;
+    io_.current_control_mode_ = ControlMode.PIVOT;
+    io_.target_arm_angle_ = angle;
+    io_.target_elevator_height_ = height;
   }
 
   /** */
   private void setEndEffectorHeight(double height, Rotation2d angle) {
-    io_.current_control_mode = ControlMode.END_EFFECTOR;
-    io_.target_arm_angle = angle;
-    io_.target_arm_height = height;
+    io_.current_control_mode_ = ControlMode.END_EFFECTOR;
+    io_.target_arm_angle_ = angle;
+    io_.target_arm_height_ = height;
   }
 
   public void setOffset(OffsetType offset_type) {
@@ -452,18 +452,17 @@ public class Elevator extends Subsystem {
 
   public class ElevatorPeriodicIo implements Logged {
     // IO container for all variables
-    @Log.File public ControlMode current_control_mode = ControlMode.PIVOT;
-    @Log.File public double current_elevator_height = 0;
-    @Log.File public double target_elevator_height = ElevatorConstants.ELEVATOR_MIN_SAFETY;
+    @Log.File public ControlMode current_control_mode_ = ControlMode.PIVOT;
+    @Log.File public double current_elevator_height_ = 0;
+    @Log.File public double target_elevator_height_ = ElevatorConstants.ELEVATOR_MIN_SAFETY;
     @Log.File public double current_arm_angle_ = 0;
     @Log.File public Target target_ = Target.STOW;
-    @Log.File public Rotation2d target_arm_angle = Rotation2d.fromDegrees(-90);
+    @Log.File public Rotation2d target_arm_angle_ = Rotation2d.fromDegrees(-90);
     @Log.File public double elevator_offset_ = 0;
     @Log.File public Rotation2d arm_offset_ = Rotation2d.fromDegrees(0);
-    @Log.File public double target_arm_height = 0;
+    @Log.File public double target_arm_height_ = 0;
     @Log.File public double elevator_master_rotations_ = 0;
     @Log.File public double elevator_follower_rotations_ = 0;
-    @Log.File public double elevator_average_position = 0;
   }
 
   /** Get logging object from subsystem */
