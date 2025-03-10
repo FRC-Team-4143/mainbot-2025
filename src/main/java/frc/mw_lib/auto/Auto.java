8094ba9a614943f3e56961759fc6e9c0e0b03448
Armolad <n.p.evans@students.mths.us>
2025-03-10 17:52:59 -0500

    adds getTrajectory to Auto

--- src/main/java/frc/mw_lib/auto/Auto.java
+++ src/main/java/frc/mw_lib/auto/Auto.java
@@ -1,8 +1,11 @@
 package frc.mw_lib.auto;
 
 import choreo.auto.AutoRoutine;
+import edu.wpi.first.math.trajectory.Trajectory;
 
 public interface Auto {
 
   public AutoRoutine getAutoRoutine();
+
+  public Trajectory[] getTrajectorys();
 }
--- src/main/java/frc/robot/autos/autoTest1.java
+++ src/main/java/frc/robot/autos/autoTest1.java
@@ -3,14 +3,16 @@ package frc.robot.autos;
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.mw_lib.auto.Auto;
 import frc.robot.AutoManager;
 import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.SwerveDrivetrain.DriveMode;
 
-public class autoTest1 {
+public class autoTest1 implements Auto {
 
-  public static AutoRoutine getAutoRoutine() {
+  public AutoRoutine getAutoRoutine() {
 
     AutoFactory autoFactory = AutoManager.getInstance().getAutoFactory();
     AutoRoutine routine = autoFactory.newRoutine("autoTest1");
@@ -23,5 +25,9 @@ public class autoTest1 {
             Commands.runOnce(() -> SwerveDrivetrain.getInstance().setDriveMode(DriveMode.IDLE)));
 
     return routine;
+  }
+
+  public Trajectory[] getTrajectorys() {
+    return new Trajectory[1];
   }
 }
