4d4fd9049e868af12181289a197c13e40c3a16f5
Nathan E. <94417916+Armolad@users.noreply.github.com>
2025-03-11 18:34:00 +0000

    adds arrayList

--- src/main/java/frc/robot/autos/autoTest1.java
+++ src/main/java/frc/robot/autos/autoTest1.java
@@ -9,14 +9,18 @@ import frc.mw_lib.auto.Auto;
 import frc.robot.AutoManager;
 import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.SwerveDrivetrain.DriveMode;
+import java.util.ArrayList;
 
 public class autoTest1 implements Auto {
+
+  private static ArrayList<AutoTrajectory> autoTrajectorys = new ArrayList<>();
 
   public AutoRoutine getAutoRoutine() {
 
     AutoFactory autoFactory = AutoManager.getInstance().getAutoFactory();
     AutoRoutine routine = autoFactory.newRoutine("autoTest1");
     AutoTrajectory driveforward = routine.trajectory("Move Forward");
+    autoTrajectorys.add(driveforward);
 
     routine.active().onTrue(Commands.sequence(driveforward.resetOdometry(), driveforward.cmd()));
     driveforward
