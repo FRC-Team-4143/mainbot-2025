163744fbc3f094d28f865fc31d94e25370ad9689
Jaxb1 <j.g.bequette@students.mths.us>
2025-03-11 20:03:55 -0600

    tuning auto and det points

--- src/main/java/frc/robot/autos/autoTest1.java
+++ src/main/java/frc/robot/autos/autoTest1.java
@@ -4,7 +4,6 @@ import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.AutoManager;
 import frc.robot.commands.AutoCoralReefScore;
 import frc.robot.commands.CoralLoad;
@@ -21,21 +20,16 @@ public class autoTest1 {
     AutoTrajectory Mid_to_IJ = routine.trajectory("Blue Mid to IJ");
     AutoTrajectory IJ_to_LeftStation = routine.trajectory("IJ to Left Station");
     AutoTrajectory LeftStation_to_KL = routine.trajectory("Left Station to KL");
-    Trigger kill_Mid_to_IJ = Mid_to_IJ.atPose("Kill", 0.5, 2);
-    Trigger kill_LeftStation_to_KL = LeftStation_to_KL.atPose("Kill", 0.5, 2);
 
-    routine.active().onTrue(Mid_to_IJ.cmd().until(kill_Mid_to_IJ::getAsBoolean));
+    routine.active().onTrue(Mid_to_IJ.cmd());
     Mid_to_IJ.inactive()
         .onTrue(
             Commands.sequence(
-                new AutoCoralReefScore(ReefScoringTarget.L3, Column.RIGHT),
+                new AutoCoralReefScore(ReefScoringTarget.L4, Column.RIGHT),
                 IJ_to_LeftStation.cmd()));
     IJ_to_LeftStation.done()
-        .onTrue(
-            Commands.sequence(
-                new CoralLoad().withTimeout(5),
-                LeftStation_to_KL.cmd().until(kill_LeftStation_to_KL::getAsBoolean)));
-    LeftStation_to_KL.inactive().onTrue(new AutoCoralReefScore(ReefScoringTarget.L3, Column.LEFT));
+        .onTrue(Commands.sequence(new CoralLoad().withTimeout(5), LeftStation_to_KL.cmd()));
+    LeftStation_to_KL.inactive().onTrue(new AutoCoralReefScore(ReefScoringTarget.L4, Column.LEFT));
 
     return routine;
   }
