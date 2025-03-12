93b167aee0b2ccf39798b5a58bfce956a6d92e3d
OliverTruelove <o.d.truelove@students.mths.us>
2025-03-12 18:34:39 -0500

    disabled game state manager on disavledInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,6 +9,8 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.lib.FieldRegions;
+import frc.robot.subsystems.GameStateManager;
+import frc.robot.subsystems.GameStateManager.RobotState;
 import frc.robot.subsystems.SwerveDrivetrain;
 import frc.robot.subsystems.SwerveDrivetrain.DriveMode;
 import java.util.Optional;
@@ -37,6 +39,9 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledInit() {
     SwerveDrivetrain.getInstance().setDriveMode(DriveMode.IDLE);
+    if(GameStateManager.getInstance().getRobotState() != RobotState.END){
+      GameStateManager.getInstance().setRobotState(RobotState.END);
+    }
   }
 
   @Override
