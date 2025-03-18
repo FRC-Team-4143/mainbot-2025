a663f99a435eb02cdeb3e5491080e39b1ced8eab
ColeHunt <huntcr2001@gmail.com>
2025-03-17 19:59:27 -0500

    adds new command for noreq sequence

---
+++ src/main/java/frc/mw_lib/command/NoReqSequentialCommandGroup.java
@@ -0,0 +1,56 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.mw_lib.command;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+import java.util.ArrayList;
+
+public class NoReqSequentialCommandGroup extends Command {
+
+  private ArrayList<Command> sequence = new ArrayList<>();
+  private int index = 0;
+
+  public NoReqSequentialCommandGroup(Command... commands) {
+    for (Command command : commands) {
+      sequence.add(command);
+    }
+  }
+
+  public NoReqSequentialCommandGroup(){}
+
+  public void addCommands(Command... commands){
+    for (Command command : commands) {
+      sequence.add(command);
+    }
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    CommandScheduler.getInstance().schedule(sequence.get(index));
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if(sequence.get(index).isFinished()){
+      index++;
+      CommandScheduler.getInstance().schedule(sequence.get(index));
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return sequence.get(sequence.size() - 1).isFinished();
+  }
+}
