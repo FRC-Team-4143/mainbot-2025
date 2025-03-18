906cf79fd74bcef730a5eed0ae8afbfc277957f2
ColeHunt <huntcr2001@gmail.com>
2025-03-17 21:20:20 -0500

    Check the index against the size of the sequence before finishing

--- src/main/java/frc/mw_lib/command/NoReqSequentialCommandGroup.java
+++ src/main/java/frc/mw_lib/command/NoReqSequentialCommandGroup.java
@@ -13,13 +13,11 @@ public class NoReqSequentialCommandGroup extends Command {
   private ArrayList<Command> sequence = new ArrayList<>();
   private int index = 0;
 
-  public NoReqSequentialCommandGroup(Command... commands) {
-    for (Command command : commands) {
-      sequence.add(command);
-    }
-  }
-
   public NoReqSequentialCommandGroup() {}
+
+  public NoReqSequentialCommandGroup(Command... commands) {
+    addCommands(commands);
+  }
 
   public void addCommands(Command... commands) {
     for (Command command : commands) {
@@ -30,6 +28,7 @@ public class NoReqSequentialCommandGroup extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    index = 0;
     CommandScheduler.getInstance().schedule(sequence.get(index));
   }
 
@@ -44,11 +43,15 @@ public class NoReqSequentialCommandGroup extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    for (Command command : sequence) {
+      CommandScheduler.getInstance().cancel(command);
+    }
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return sequence.get(sequence.size() - 1).isFinished();
+    return index == (sequence.size() - 1) && sequence.get(sequence.size() - 1).isFinished();
   }
 }
