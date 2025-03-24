b1e269a5704a8d3bcbd3e199941204e474fb1916
Jaxb1 <j.g.bequette@students.mths.us>
2025-03-23 21:15:42 -0500

    intakefeed cmd

--- src/main/java/frc/robot/subsystems/Pickup.java
+++ src/main/java/frc/robot/subsystems/Pickup.java
@@ -70,8 +70,10 @@ public class Pickup extends Subsystem {
   }
 
   /**
-   * This function should be logic and code to fully reset your subsystem. This is called during
-   * initialization, and should handle I/O configuration and initializing data members.
+   * This function should be logic and code to fully reset your subsystem. This is
+   * called during
+   * initialization, and should handle I/O configuration and initializing data
+   * members.
    */
   @Override
   public void reset() {
@@ -79,8 +81,10 @@ public class Pickup extends Subsystem {
   }
 
   /**
-   * Inside this function, all of the SENSORS should be read into variables stored in the PeriodicIO
-   * class defined below. There should be no calls to output to actuators, or any logic within this
+   * Inside this function, all of the SENSORS should be read into variables stored
+   * in the PeriodicIO
+   * class defined below. There should be no calls to output to actuators, or any
+   * logic within this
    * function.
    */
   @Override
@@ -89,8 +93,10 @@ public class Pickup extends Subsystem {
   }
 
   /**
-   * Inside this function, all of the LOGIC should compute updates to output variables in the
-   * PeriodicIO class defined below. There should be no calls to read from sensors or write to
+   * Inside this function, all of the LOGIC should compute updates to output
+   * variables in the
+   * PeriodicIO class defined below. There should be no calls to read from sensors
+   * or write to
    * actuators in this function.
    */
   @Override
@@ -119,8 +125,10 @@ public class Pickup extends Subsystem {
   }
 
   /**
-   * Inside this function actuator OUTPUTS should be updated from data contained in the PeriodicIO
-   * class defined below. There should be little to no logic contained within this function, and no
+   * Inside this function actuator OUTPUTS should be updated from data contained
+   * in the PeriodicIO
+   * class defined below. There should be little to no logic contained within this
+   * function, and no
    * sensors should be read.
    */
   @Override
@@ -130,9 +138,12 @@ public class Pickup extends Subsystem {
   }
 
   /**
-   * Inside this function telemetry should be output to smartdashboard. The data should be collected
-   * out of the PeriodicIO class instance defined below. There should be no sensor information read
-   * in this function nor any outputs made to actuators within this function. Only publish to
+   * Inside this function telemetry should be output to smartdashboard. The data
+   * should be collected
+   * out of the PeriodicIO class instance defined below. There should be no sensor
+   * information read
+   * in this function nor any outputs made to actuators within this function. Only
+   * publish to
    * smartdashboard here.
    */
   @Override
@@ -166,11 +177,16 @@ public class Pickup extends Subsystem {
   public void setPivotPosition(double value) {
     pivot_motor_.setPosition(value);
   }
+
   public class PickupPeriodicIo implements Logged {
-    @Log.File public PickupMode current_mode_ = PickupMode.DEPLOYED;
-    @Log.File public double target_intake_speed_ = 0;
-    @Log.File public double current_pivot_angle = 0;
-    @Log.File public double target_pivot_angle = 0;
+    @Log.File
+    public PickupMode current_mode_ = PickupMode.DEPLOYED;
+    @Log.File
+    public double target_intake_speed_ = 0;
+    @Log.File
+    public double current_pivot_angle = 0;
+    @Log.File
+    public double target_pivot_angle = 0;
   }
 
   @Override
