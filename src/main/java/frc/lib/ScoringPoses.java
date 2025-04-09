8d765a6e6111e5ccfabfae9951ff12ca0202c477
ColeHunt <huntcr2001@gmail.com>
2025-04-09 16:17:19 -0500

    adds opposing algae scoring poses

--- src/main/java/frc/lib/ScoringPoses.java
+++ src/main/java/frc/lib/ScoringPoses.java
@@ -55,27 +55,39 @@ public class ScoringPoses {
       FieldConstants.Reef.CENTER_FACES[4].transformBy(REEF_FACE_OFFSET);
   public static Pose2d REEF_FACE_5_POSE =
       FieldConstants.Reef.CENTER_FACES[5].transformBy(REEF_FACE_OFFSET);
-    
-// Poses used for Stealing Algae from Opposing Reef
-public static Pose2d OPP_REEF_FACE_0_POSE =
-      AllianceFlipUtil.apply(FieldConstants.Reef.CENTER_FACES[0].transformBy(REEF_FACE_OFFSET), FieldConstants.SYMMETRY_TYPE);
-  public static Pose2d OPP_REEF_FACE_1_POSE =
-      AllianceFlipUtil.apply(FieldConstants.Reef.CENTER_FACES[1].transformBy(REEF_FACE_OFFSET), FieldConstants.SYMMETRY_TYPE);
-  public static Pose2d OPP_REEF_FACE_2_POSE =
-      AllianceFlipUtil.apply(FieldConstants.Reef.CENTER_FACES[2].transformBy(REEF_FACE_OFFSET), FieldConstants.SYMMETRY_TYPE);
-  public static Pose2d OPP_REEF_FACE_3_POSE =
-      AllianceFlipUtil.apply(FieldConstants.Reef.CENTER_FACES[3].transformBy(REEF_FACE_OFFSET), FieldConstants.SYMMETRY_TYPE);
-  public static Pose2d OPP_REEF_FACE_4_POSE =
-      AllianceFlipUtil.apply(FieldConstants.Reef.CENTER_FACES[4].transformBy(REEF_FACE_OFFSET), FieldConstants.SYMMETRY_TYPE);
-  public static Pose2d OPP_REEF_FACE_5_POSE =
-      AllianceFlipUtil.apply(FieldConstants.Reef.CENTER_FACES[5].transformBy(REEF_FACE_OFFSET), FieldConstants.SYMMETRY_TYPE);
 
-// Coral Station Poses
+  // Poses used for Stealing Algae from Opposing Reef
+  public static Pose2d OPP_REEF_FACE_0_POSE =
+      AllianceFlipUtil.apply(
+          FieldConstants.Reef.CENTER_FACES[0].transformBy(REEF_FACE_OFFSET),
+          FieldConstants.SYMMETRY_TYPE);
+  public static Pose2d OPP_REEF_FACE_1_POSE =
+      AllianceFlipUtil.apply(
+          FieldConstants.Reef.CENTER_FACES[1].transformBy(REEF_FACE_OFFSET),
+          FieldConstants.SYMMETRY_TYPE);
+  public static Pose2d OPP_REEF_FACE_2_POSE =
+      AllianceFlipUtil.apply(
+          FieldConstants.Reef.CENTER_FACES[2].transformBy(REEF_FACE_OFFSET),
+          FieldConstants.SYMMETRY_TYPE);
+  public static Pose2d OPP_REEF_FACE_3_POSE =
+      AllianceFlipUtil.apply(
+          FieldConstants.Reef.CENTER_FACES[3].transformBy(REEF_FACE_OFFSET),
+          FieldConstants.SYMMETRY_TYPE);
+  public static Pose2d OPP_REEF_FACE_4_POSE =
+      AllianceFlipUtil.apply(
+          FieldConstants.Reef.CENTER_FACES[4].transformBy(REEF_FACE_OFFSET),
+          FieldConstants.SYMMETRY_TYPE);
+  public static Pose2d OPP_REEF_FACE_5_POSE =
+      AllianceFlipUtil.apply(
+          FieldConstants.Reef.CENTER_FACES[5].transformBy(REEF_FACE_OFFSET),
+          FieldConstants.SYMMETRY_TYPE);
+
+  // Coral Station Poses
   public static Pose2d LEFT_CORAL_STATION_POSE =
       FieldConstants.CoralStation.LEFT_CENTER_FACE.transformBy(CORAL_STATION_OFFSET);
   public static Pose2d RIGHT_CORAL_STATION_POSE =
       FieldConstants.CoralStation.RIGHT_CENTER_FACE.transformBy(CORAL_STATION_OFFSET);
-// Barge Poses
+  // Barge Poses
   public static Pose2d BARGE_TIGHT_ROPE_POSE_A =
       new Pose2d(7.7562, FieldConstants.FIELD_WIDTH, Rotation2d.fromDegrees(0));
   public static Pose2d BARGE_TIGHT_ROPE_POSE_B =
@@ -86,9 +98,8 @@ public static Pose2d OPP_REEF_FACE_0_POSE =
               - Constants.ClawConstants.ALGAE_IMP_OFFSET
               + (FieldConstants.ALGAE_DIAMETER / 2),
           Rotation2d.fromDegrees(0));
-  public static TightRope BARGE_TIGHT_ROPE =
-      new TightRope(BARGE_TIGHT_ROPE_POSE_A, BARGE_TIGHT_ROPE_POSE_B, "Barge");
-// Processor Poses
+
+  // Processor Poses
   public static Pose2d PROCESSOR_TIGHT_ROPE_POSE_A =
       FieldConstants.Processor.CENTER_FACE.transformBy(
           new Transform2d(
@@ -98,6 +109,10 @@ public static Pose2d OPP_REEF_FACE_0_POSE =
   public static Pose2d PROCESSOR_TIGHT_ROPE_POSE_B =
       FieldConstants.Processor.CENTER_FACE.transformBy(
           new Transform2d(0, -Constants.ClawConstants.ALGAE_IMP_OFFSET, new Rotation2d()));
+
+  // Tight Ropes
+  public static TightRope BARGE_TIGHT_ROPE =
+      new TightRope(BARGE_TIGHT_ROPE_POSE_A, BARGE_TIGHT_ROPE_POSE_B, "Barge");
   public static TightRope PROCESSOR_TIGHT_ROPE =
       new TightRope(PROCESSOR_TIGHT_ROPE_POSE_A, PROCESSOR_TIGHT_ROPE_POSE_B, "Processor");
 }
