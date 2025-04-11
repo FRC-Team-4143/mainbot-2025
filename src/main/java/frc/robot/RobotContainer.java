ebd32a9ed4b3047bbfe69f4ccc7811ac1a414240
Nathan E. <94417916+Armolad@users.noreply.github.com>
2025-04-11 13:22:52 +0000

    unregester sub systems

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -21,12 +21,12 @@ public class RobotContainer extends SubsystemManager {
     // !!!!!! ALL SUBSYSTEMS MUST BE REGISTERED HERE TO RUN !!!!!!!
     registerSubsystem(SwerveDrivetrain.getInstance());
     registerSubsystem(PoseEstimator.getInstance());
-    registerSubsystem(Claw.getInstance());
-    registerSubsystem(Elevator.getInstance());
-    registerSubsystem(Climber.getInstance());
-    registerSubsystem(GameStateManager.getInstance());
-    registerSubsystem(Pickup.getInstance());
-    registerSubsystem(CoralDetector.getInstance());
+    //registerSubsystem(Claw.getInstance());
+    //registerSubsystem(Elevator.getInstance());
+    //registerSubsystem(Climber.getInstance());
+    //registerSubsystem(GameStateManager.getInstance());
+    //registerSubsystem(Pickup.getInstance());
+    //registerSubsystem(CoralDetector.getInstance());
 
     // !!!!! LEAVE THESE LINES AS THE LAST LINE IN THE CONSTRUCTOR !!!!!!
     reset();
